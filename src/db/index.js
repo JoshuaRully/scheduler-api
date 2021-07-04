8409fb95af0041a720043d162683f83511136c58
Joshua Rully <joshrully@gmail.com>
2021-07-04 15:58:51 -0400

    fix conflict

--- src/db/index.js
+++ src/db/index.js
@@ -1,8 +1,12 @@
 const pg = require("pg");
 
 const client = new pg.Client({
+<<<<<<< HEAD
   connectionString: process.env.DATABASE_URL || "",
   ssl: { rejectUnauthorized: false }
+=======
+  connectionString: process.env.DATABASE_URL || ""
+>>>>>>> parent of 2f14bbe... adjust db connection for possible fix
 });
 
 client
