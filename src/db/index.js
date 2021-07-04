2f14bbe00794b9d379a79ceb54f9c231b6b7777e
Joshua Rully <joshrully@gmail.com>
2021-07-04 15:18:20 -0400

    adjust db connection for possible fix

--- src/db/index.js
+++ src/db/index.js
@@ -1,7 +1,12 @@
 const pg = require("pg");
 
 const client = new pg.Client({
-  connectionString: process.env.DATABASE_URL || ""
+  user: 'aqauifrrqfwbax',
+  password: '3055ea7f0b7d57a845e7cfc43507a549a4e3cf0d0a4769585df2d7ae8dd15e29',
+  database: 'dcc27l0c0hm44e',
+  port: 5432,
+  host: 'ec2-3-231-69-204.compute-1.amazonaws.com',
+  ssl: true
 });
 
 client
